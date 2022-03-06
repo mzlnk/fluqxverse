a0f5283dfddd6db3aaa835a2b312ecaceb3b514a
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2022-03-06 23:24:00 +0100

    added project sources (server + ui)

---
+++ server/src/main/java/io/mzlnk/oauth2/exchange/broker/ServerApplication.java
@@ -0,0 +1,13 @@
+package io.mzlnk.oauth2.exchange.broker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/io/mzlnk/oauth2/exchange/broker/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mzlnk.oauth2.exchange.broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
