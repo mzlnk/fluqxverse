0de3493304072f02daf21e2a8c8767fae985b3bd
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2022-03-07 00:24:23 +0100

    configured maven project (groovy support, etc.)

--- server/src/test/groovy/io/mzlnk/oauth2/exchange/broker/ApplicationTest.groovy
+++ server/src/test/groovy/io/mzlnk/oauth2/exchange/broker/ApplicationTest.groovy
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ServerApplicationTests {
+class ApplicationTest {
 
     @Test
-    void contextLoads() {
+    void 'context loads'() {
     }
 
 }
