0551551fc2766711223a5c52445cfaebbfe5eb7c
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2022-03-13 10:57:58 +0100

    refactored project structure

--- server/src/main/java/io/mzlnk/authn/identity/broker/Application.java
+++ server/src/main/java/io/mzlnk/authn/identity/broker/Application.java
@@ -1,4 +1,4 @@
-package io.mzlnk.oauth2.exchange.broker;
+package io.mzlnk.authn.identity.broker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- server/src/test/groovy/io/mzlnk/authn/identity/broker/ApplicationTest.groovy
+++ server/src/test/groovy/io/mzlnk/authn/identity/broker/ApplicationTest.groovy
@@ -1,4 +1,4 @@
-package io.mzlnk.oauth2.exchange.broker;
+package io.mzlnk.authn.identity.broker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
