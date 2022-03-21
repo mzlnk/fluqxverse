4f82c1bdd27c86d6ba2ad47acfcad30e22ab0684
Marcin Zielonka <dev.zielonka.marcin@gmail.com>
2022-03-21 21:28:00 +0100

    added sources for authn and authz

---
+++ authn/src/main/java/io/mzlnk/authn/AuthnApplication.java
@@ -0,0 +1,13 @@
+package io.mzlnk.authn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthnApplication.class, args);
+    }
+
+}
